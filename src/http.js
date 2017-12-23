4af2c654ab469cb03bf999b416a07f2facc2d583
tbreuss <thomasbreuss@gmx.ch>
2017-12-23 11:45:29 +0100

    Added (empty) interceptors

--- src/http.js
+++ src/http.js
@@ -2,10 +2,30 @@
 
 import axios from 'axios';
 
-export const HTTP = axios.create({
+var instance = axios.create({
     baseURL: 'http://testing.tebe.ch/wissen-api/public/api.php/',
-    //baseURL: 'http://jsonplaceholder.typicode.com/',
+    //baseURL: 'http://localhost:8999/api.php/',
     headers: {
         //Authorization: 'Bearer {token}'
     }
-})
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response
+}, function (error) {
+    // Do something with response error
+    return Promise.reject(error)
+});
+
+export const HTTP = instance;
