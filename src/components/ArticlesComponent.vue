259bbb99e4930d59db6704ac1f682fc605c29479
tbreuss <thomasbreuss@gmx.ch>
2017-12-30 08:53:14 +0100

    Using slots in articles component

--- src/components/ArticlesComponent.vue
+++ src/components/ArticlesComponent.vue
@@ -1,23 +1,21 @@
 <template>
-    <div v-cloak>
-        <h4>{{ title }}</h4>
+    <div>
+        <slot name="title"><h4>Artikel Componente</h4></slot>
         <div class="loading" v-if="loading">
             Lade...
         </div>
-        <div v-else>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="list-group">
-                        <router-link v-for="article in articles" :to="'/article/' + article.id"
-                                     class="list-group-item d-flex justify-content-between align-items-center"
-                                     :key="article.id">{{ article.title }}
-                            <span v-if="article.views" class="badge badge-secondary">{{ article.views }}</span>
-                            <span v-if="article.modified" class="badge badge-secondary">{{ article.modified | date
-                                }}</span>
-                            <span v-if="article.created" class="badge badge-secondary">{{ article.created | date
-                                }}</span>
-                        </router-link>
-                    </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="list-group">
+                    <router-link v-for="article in articles" :to="'/article/' + article.id"
+                                 class="list-group-item d-flex justify-content-between align-items-center"
+                                 :key="article.id">{{ article.title }}
+                        <span v-if="article.views" class="badge badge-secondary">{{ article.views }}</span>
+                        <span v-if="article.modified" class="badge badge-secondary">{{ article.modified | date
+                            }}</span>
+                        <span v-if="article.created" class="badge badge-secondary">{{ article.created | date
+                            }}</span>
+                    </router-link>
                 </div>
             </div>
         </div>
@@ -32,20 +30,6 @@
       return {
         articles: [],
         loading: false
-      }
-    },
-    computed: {
-      title: function () {
-        if (this.mode == 'popular') {
-          return 'Die beliebtesten Artikel'
-        }
-        if (this.mode == 'latest') {
-          return 'Die neuesten Artikel'
-        }
-        if (this.mode == 'modified') {
-          return 'Die aktuellsten Artikel'
-        }
-        return ''
       }
     },
     props: ['mode'],
--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="hello">
-        <articles :mode="'popular'"></articles>
-        <articles :mode="'latest'"></articles>
-        <articles :mode="'modified'"></articles>
+        <articles :mode="'popular'"><h4 slot="title">Die beliebtesten Artikel</h4></articles>
+        <articles :mode="'latest'"><h4 slot="title">Die neuesten Artikel</h4></articles>
+        <articles :mode="'modified'"><h4 slot="title">Die aktuellsten Artikel</h4></articles>
     </div>
 </template>
 
